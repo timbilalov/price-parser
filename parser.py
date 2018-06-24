44b5e04d31b6cb45b9e19b2a89b9ed65f7316d25
unfriend <t-moore@mail.ru>
2018-06-24 18:24:19 +0300

    the dog has grown :)

--- parser.py
+++ parser.py
@@ -6,14 +6,38 @@ import gi
 gi.require_version('Notify', '0.7')
 from gi.repository import Notify
 
-page = requests.get('https://www.bethowen.ru/catalogue/dogs/korma/syxoi/korm-dlya-shchenkov-dukes-dlya-krupnykh-porod-kuritsa-sukh-12kg/')
+page = requests.get('https://www.bethowen.ru/catalogue/dogs/korma/syxoi/korm-dlya-sobak-duke-s-farm-dlya-srednikh-i-krupnykh-porod-indeyka-sukh-12kg/')
 
 source_code = page.text
 soup = BeautifulSoup(source_code, "lxml")
 
+productCodeElements = soup.findAll("h3", {"class": "product-code"})
+for elem in productCodeElements:
+	if not elem.findAll("a"):
+		parentBlock = elem.find_parents("div", {"class": "productFilter"})[0]
+
+# Error handling
+if not parentBlock:
+	Notify.init("Error while parsing prices")
+
+	summary = "Error while parsing prices"
+	body = "See and explore parser.py to find what went wrong"
+
+	notification = Notify.Notification.new(
+	    summary,
+	    body,
+	)
+
+	notification.show()
+
+	# Clean
+	Notify.uninit()
+	exit()
+
+# Successfully found parentBlock
 titleElement = soup.findAll("h1", {"class": "product-title"})
-priceElemStandart = soup.findAll("span", {"class": "price-standard"})
-priceElemSales = soup.findAll("span", {"class": "price-sales"})
+priceElemStandart = parentBlock.findAll("span", {"class": "price-standard"})
+priceElemSales = parentBlock.findAll("span", {"class": "price-sales"})
 
 title = titleElement[0].contents[0].strip() if len(titleElement) > 0 else False
 priceStandart = (int)(priceElemStandart[0].contents[0].strip()) if len(priceElemStandart) > 0 else False
@@ -21,9 +45,6 @@ priceSales = (int)(priceElemSales[0].contents[0].strip()) if len(priceElemSales)
 if not priceStandart and priceSales > 0:
     priceStandart = priceSales
     priceSales = False
-# print title
-# print priceStandart
-# print priceSales
 
 Notify.init("Prices")
 
