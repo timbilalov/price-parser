e4b76e921c25d86be53912034e865553e9a5ed7c
unfriend <t-moore@mail.ru>
2019-09-03 23:31:30 +0300

    4lapy.ru site markup changed, again

--- parser.py
+++ parser.py
@@ -5,6 +5,7 @@ import requests
 import gi
 gi.require_version('Notify', '0.7')
 from gi.repository import Notify
+import re
 
 pageAddresses = [
 	'https://4lapy.ru/catalog/sobaki/Grandin_Fresh_Meat_suhoy_korm_dlya_sobak_krupnyh_i_gigantskih_porod_kuritsa.html?offer=44192',
@@ -16,6 +17,23 @@ pageAddresses = [
 	'https://4lapy.ru/catalog/sobaki/korm-sobaki/sukhoy-korm-sobaki/Grandin_Fresh_Meat_suhoy_korm_dlya_sobak_melkih_porod_yagnenok.html?offer=81072',
 ]
 
+# Error handling
+def fail():
+	Notify.init("Error while parsing prices")
+
+	summary = "Error while parsing prices"
+	body = "See and explore parser.py to find what went wrong"
+
+	notification = Notify.Notification.new(
+	    summary,
+	    body,
+	)
+
+	notification.show()
+
+	# Clean
+	Notify.uninit()
+
 def checkPage(pageAddress):
 	page = requests.get(pageAddress)
 
@@ -24,50 +42,37 @@ def checkPage(pageAddress):
 
 	parentBlock = soup.findAll("div", {"class": "b-product-card__top"})[0]
 
-	# Error handling
 	if not parentBlock:
-		Notify.init("Error while parsing prices")
-
-		summary = "Error while parsing prices"
-		body = "See and explore parser.py to find what went wrong"
-
-		notification = Notify.Notification.new(
-		    summary,
-		    body,
-		)
-
-		notification.show()
-
-		# Clean
-		Notify.uninit()
+		fail()
 		exit()
 
 	# Successfully found parentBlock
 	brandElement = parentBlock.findAll("span", {"itemprop": "brand"})
 	titleElement = parentBlock.findAll("h1", {"class": "b-title b-title--h1 b-title--card"})
-	priceElemStandart = soup.find("a", {"class": "b-weight-container__link js-price active-link"})
+	priceElemOld = soup.find("span", {"class": "b-advice__old-price"})
+	priceElemStandart = soup.find("span", {"class": "b-advice__cost"})
+	priceStandart = False
+	priceSales = False
+
 	if priceElemStandart:
-		priceStandart = (int)(priceElemStandart["data-oldprice"])
-		priceSales = (int)(priceElemStandart["data-price"])
-
+		priceStandart = (int)(re.sub('[^0-9]','', priceElemStandart.text.strip()))
+		if priceElemOld:
+			priceSales = priceStandart
+			priceStandart = (int)(re.sub('[^0-9]','', priceElemOld.text.strip()))
 	else:
-		priceElemStandart = soup.find("span", {"class": "b-advice__cost"})
-		if priceElemStandart:
-			priceStandart = (int)(priceElemStandart.contents[0].strip())
-			priceSales = False
+		priceElemStandart = soup.findAll("meta", itemprop = "price")
+		if priceElemStandart and len(priceElemStandart) > 0:
+			prices = []
+			for elem in priceElemStandart:
+				prices.append((int)(elem["content"]))
+			prices.sort(reverse = True)
+			priceStandart = prices[0]
 		else:
-			priceElemStandart = soup.findAll("meta", itemprop = "price")
-			if priceElemStandart and len(priceElemStandart) > 0:
-				prices = []
-				for elem in priceElemStandart:
-					prices.append((int)(elem["content"]))
-				prices.sort(reverse = True)
-				priceStandart = prices[0]
-				priceSales = False
-			else:
-				priceStandart = False
-				priceSales = False
+			priceStandart = False
 
+	if not priceStandart:
+		fail()
+		exit()
 
 	title = titleElement[0].contents[0].strip() if len(titleElement) > 0 else False
 	if len(brandElement) > 0:
